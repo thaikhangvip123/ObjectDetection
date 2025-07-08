f597fc2523c51a1d311cc56d9a17787635104dd9
thaikhangvip123 <thaikhangvip123@gmail.com>
2025-07-08 16:24:53 +0700

    ML exercise

--- Day1/test.py
+++ Day1/test.py
@@ -7,3 +7,6 @@ from scipy import ndimage
 from lr_utils import load_dataset
 
 %matplotlib inline
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
