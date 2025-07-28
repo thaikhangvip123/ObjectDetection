5fe3e3889399e2e0e8c8e515aa725fb252d60050
thaikhangvip123 <thaikhangvip123@gmail.com>
2025-07-29 01:42:19 +0700

    first training with simple dataset

---
+++ Day2/broken_defection-2/README.dataset.txt
@@ -0,0 +1,6 @@
+# broken_defection > 2025-07-28 5:02pm
+https://universe.roboflow.com/object-detection-39pwr/broken_defection-5p5pu
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ Day2/broken_defection.py
@@ -0,0 +1,23 @@
+
+######## Install Dataset ############
+
+# from roboflow import Roboflow
+# rf = Roboflow(api_key="RkzwodUVxKR7dWuHYpFi")
+# project = rf.workspace("object-detection-39pwr").project("broken_defection-5p5pu")
+# version = project.version(2)
+# dataset = version.download("yolov8")
+
+######## Training ###########
+
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8 model (or start from scratch)
+def train():
+    model = YOLO("yolov8n.pt")  # or yolov8s.pt, yolov8m.pt, etc.
+    model.train(data="E:\\ObjectDetection\\Day2\\broken_defection-2\\data.yaml",  # path to your dataset config
+                epochs=50,
+                imgsz=640,
+                batch=16)
+# Train on your custom dataset
+if __name__ == "__main__":
+    train()
