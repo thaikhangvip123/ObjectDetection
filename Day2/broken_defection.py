477b1a223b9059c8780609a10f0aec2a460a068a
thaikhangvip123 <thaikhangvip123@gmail.com>
2025-07-29 23:22:02 +0700

    second training

--- Day2/broken_defection.py
+++ Day2/broken_defection.py
@@ -1,5 +1,5 @@
 
-######## Install Dataset ############
+######## Installation Dataset ############
 
 # from roboflow import Roboflow
 # rf = Roboflow(api_key="RkzwodUVxKR7dWuHYpFi")
@@ -14,10 +14,17 @@ from ultralytics import YOLO
 # Load a pretrained YOLOv8 model (or start from scratch)
 def train():
     model = YOLO("yolov8n.pt")  # or yolov8s.pt, yolov8m.pt, etc.
-    model.train(data="E:\\ObjectDetection\\Day2\\broken_defection-2\\data.yaml",  # path to your dataset config
-                epochs=50,
-                imgsz=640,
-                batch=16)
+    model.train(
+        data="E:\\ObjectDetection\\Day2\\broken_defection-2\\data.yaml",  # path to your dataset config
+        epochs=50,
+        imgsz=640,
+        batch=16
+
+        # Lr0=0.001,
+        # weigh_decay=0.0005,
+        # dropout=0.2,
+        # patience=15
+    )
 # Train on your custom dataset
 if __name__ == "__main__":
     train()
